85bf2a7869a6dce785f16cf1b6fb3bb9e644c483
daresh887 <daresh.hanganu@gmail.com>
2026-02-14 17:10:33 +0200

    Fix syntax error in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -7,11 +7,8 @@ const nextConfig: NextConfig = {
     hostname: "khfnmacecokmwqqwuyud.supabase.co",
       pathname: "/storage/v1/object/public/**",
       },
-{
-  protocol: "https",
-    hostname: "lh3.googleusercontent.com",
-      },
     ],
+domains: ['lh3.googleusercontent.com'],
   },
 experimental: {
   serverActions: {
